3d8cadc24cbbd6ba7682492f29d99442919bd2e4
orlandoy <168177412+orlandoy@users.noreply.github.com>
2025-04-26 21:54:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,14 @@
-from dash import Dash, Input, Output, State, ctx
-import dash_bootstrap_components as dbc
+from dash import Dash
 from dashboard.layout import layout
-from dashboard.charts import generate_bar_chart
-from dashboard.table import generate_table
+from dashboard.callbacks import register_callbacks
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = "动态增删改查 Dash 示例"
+app = Dash(__name__, external_stylesheets=["/assets/style.css"])
+app.title = "智元A2 项目看板"
 app.layout = layout
 
-# 更新图表和表格
-@app.callback(
-    Output('chart-container', 'children'),
-    Output('table-container', 'children'),
-    Output('stored-data', 'data'),
-    Input('data-table', 'data'),
-    Input('add-row-btn', 'n_clicks'),
-    State('stored-data', 'data'),
-)
-def update_output(table_data, n_clicks, stored_data):
-    trigger = ctx.triggered_id
+register_callbacks(app)
 
-    if trigger == 'add-row-btn':
-        stored_data.append({"项目名称": "", "采集时间": "", "采集数量": "0", "状态": "", "上传": ""})
-    else:
-        stored_data = table_data
-
-    return generate_bar_chart(stored_data), generate_table(stored_data), stored_data
-
-import os
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8050))
-    app.run(debug=True, host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=port, debug=True)
