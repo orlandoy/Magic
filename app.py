4937d1458c37fbf4f161ac69447a6f1df900461c
orlandoy <168177412+orlandoy@users.noreply.github.com>
2025-04-26 22:29:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from dashboard.layout import layout
+import os
 import dash
+from dashboard.layout import layout
 
 app = dash.Dash(__name__, external_stylesheets=[
     "https://fonts.googleapis.com/css2?family=Orbitron&display=swap",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 ])
-app.title = "智元A2项目数据采集"
+app.title = "智元A2项目数据采集看板"
 
 app.layout = layout
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8050)))
-
